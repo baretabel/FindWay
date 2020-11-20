247f7b418c2d8788dbe97ca622fb387276aa3b3b
baretabel <baretabel4@gmail.com>
2020-11-20 16:06:07 +0400

    node

--- Serveur/app.js
+++ Serveur/app.js
@@ -2,22 +2,31 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const turf = require('@spatial/intersect')
+const turf = require('@turf/turf')
 const fs = require('fs')
 
 fs.readFile('./json/rue.geojson', function(erreur, fichier) {
-    let rues = JSON.parse(fichier)
-    
-    console.log(rues.features[0]
-        )
- })
+  for (let i = 0; i < rues.length; i++) {
+    for (let y = 0; y < rues.length; y++)
+    rue1=rues.features[i].geometry.coordinates
+    rue2=rues.features[y].geometry.coordinates
+    var poly1 = turf.multiLineString(rue1);
+    var poly2 = turf.multiLineString(rue2);
+    var intersection = turf.intersect(poly1, poly2);
+    console.log(intersection);
+  }
+  /*let rues = JSON.parse(fichier)
+  rue1=rues.features[188].geometry.coordinates
+  rue2=rues.features[27].geometry.coordinates
+  var poly1 = turf.multiLineString(rue1);
+  var poly2 = turf.multiLineString(rue2);
+  var intersection = turf.intersect(poly1, poly2);
+  console.log(intersection);*/
+})
 
 
 app.get('/', (req, res) => {
-    fs.readFile('./json/rue.geojson', function(erreur, fichier) {
-        let rues = JSON.parse(fichier)
-        console.log(rues.features.properties)
-     })
+    
 })
 
 app.listen(port, () => {
