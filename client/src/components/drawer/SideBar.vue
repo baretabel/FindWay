1cb58b42b1c43236e739ad0d49ccb19750073583
morgan <morgan@seyes.re>
2020-11-30 14:06:46 +0400

    Added btn-toggle and change icon for sidebar

--- client/src/components/drawer/SideBar.vue
+++ client/src/components/drawer/SideBar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <v-btn icon elevation="3" id="btn-sideBar" class="btn-transparent" @click="drawer = !drawer">
-      <v-icon class="md-36">mdi-menu</v-icon>
+      <v-icon>mdi-loupe</v-icon>
     </v-btn>
     <v-navigation-drawer v-model="drawer" absolute temporary right id="sideBar" :width="500" class="navigation-drawer">
       <div id="sidebar-body" class="sideBar-items d-flex flex-column align-content-space-around justify-center">
@@ -11,15 +11,17 @@
           </v-btn>
         </div>
         <div class="d-flex flex-row align-center justify-center">
-          <v-btn icon id="walk-path" class="btn-transparent" rounded @click="pathChoice('walk')">
-            <v-icon>mdi-walk</v-icon>
-          </v-btn>
-          <v-btn icon id="bike-path" class="btn-transparent" rounded @click="pathChoice('bike')">
-            <v-icon>mdi-bike</v-icon>
-          </v-btn>
-          <v-btn icon id="car-path" class="btn-transparent" rounded @click="pathChoice('car')">
-            <v-icon>mdi-car</v-icon>
-          </v-btn>
+          <v-btn-toggle class="btn-toggle" v-model="path">
+            <v-btn icon id="walk-path" class="btn-transparent" rounded value="walk" @click="selectStylePath()">
+              <v-icon>mdi-walk</v-icon>
+            </v-btn>
+            <v-btn icon id="bike-path" class="btn-transparent" rounded value="bike" @click="selectStylePath()">
+              <v-icon>mdi-bike</v-icon>
+            </v-btn>
+            <v-btn icon id="car-path" class="btn-transparent" rounded value="car" @click="selectStylePath()">
+              <v-icon>mdi-car</v-icon>
+            </v-btn>
+          </v-btn-toggle>
         </div>
         <br>
         <div class="row">
@@ -60,6 +62,7 @@
       return {
         drawer: false,
         model: null,
+        path: undefined,
         states: [
           'Alabama', 'Alaska', 'American Samoa', 'Arizona',
           'Arkansas', 'California', 'Colorado', 'Connecticut',
@@ -81,31 +84,6 @@
     methods: {
       close() {
         this.drawer = false;
-      },
-      pathChoice(path) {
-        console.log(path)
-        let walkPath = document.getElementById('walk-path')
-        let bikePath = document.getElementById('bike-path')
-        let carPath = document.getElementById('car-path')
-
-        if(path === 'walk') {
-          walkPath.classList.add('select-path');
-          bikePath.classList.remove('select-path');
-          carPath.classList.remove('select-path');
-        }
-        if(path === 'bike') {
-          bikePath.classList.add('select-path');
-          walkPath.classList.remove('select-path');
-          carPath.classList.remove('select-path');
-        }
-        if(path === 'car') {
-          carPath.classList.add('select-path');
-          walkPath.classList.remove('select-path');
-          bikePath.classList.remove('select-path');
-        }
-      },
-      actions: {
-
       }
     }
   }
@@ -134,21 +112,20 @@
 }
 
 #btn-sideBar {
-  background: transparent;
+  background: white;
   margin: 10px !important;
+  transform: rotate(-90deg);
 }
 
-.select-path {
-  border: 0.5px green solid;
-}
-.select-path i {
-  color: green !important;
+.btn-toggle {
+  background: none !important;
 }
 
 .btn-transparent {
   margin-right: 5px;
   background: none !important;
   box-shadow: none !important;
+  border-radius: 25px !important;
 }
 
 #btn-sideBar i {
@@ -183,5 +160,13 @@
   margin-left: 10px;
   margin-right: 10px;
   width: 100%;
+}
+
+.v-item--active, .v-btn--active {
+  background: lightgreen !important;
+}
+
+.v-item--active i, .v-btn--active i {
+  color: darkgreen !important;
 }
 </style>
