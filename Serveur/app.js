573fa196601f305393d587032aba410913b69ffc
baretabel <baretabel4@gmail.com>
2020-11-30 08:58:55 +0400

    intersection

--- Serveur/app.js
+++ Serveur/app.js
@@ -6,21 +6,38 @@ const turf = require('@turf/turf')
 const fs = require('fs')
 
 fs.readFile('./json/rue.geojson', function(erreur, fichier) {
-  for (let i = 0; i < rues.length; i++) {
-    for (let y = 0; y < rues.length; y++)
+  let intersections=[];
+  let rues = JSON.parse(fichier);
+
+  for (let i = 0; i < rues.features.length; i++) {
+    for (let y = 0; y < rues.features.length; y++){
     rue1=rues.features[i].geometry.coordinates
     rue2=rues.features[y].geometry.coordinates
     var poly1 = turf.multiLineString(rue1);
     var poly2 = turf.multiLineString(rue2);
     var intersection = turf.intersect(poly1, poly2);
-    console.log(intersection);
+    
+    if(intersection){
+      intersections+=intersection;
+      console.log(intersection);
+    }
+    
+    }
   }
-  /*let rues = JSON.parse(fichier)
+  var string = JSON.stringify(intersections,null,'\t');
+
+     fs.writeFile('./intersection.geojson',string,function(err) {
+       if(err) return console.error(err);
+       console.log('done');
+     })
+  console.log(intersections);
+  /*
   rue1=rues.features[188].geometry.coordinates
   rue2=rues.features[27].geometry.coordinates
   var poly1 = turf.multiLineString(rue1);
   var poly2 = turf.multiLineString(rue2);
   var intersection = turf.intersect(poly1, poly2);
+  
   console.log(intersection);*/
 })
 
