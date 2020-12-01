f6336e506afa70be352897d27f97e11861a0d868
morgan <morgan@seyes.re>
2020-12-01 13:33:02 +0400

    Added new style for sidebar

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,7 @@
 
     <div>
         <v-container class="fill-height">
-            <SideBar />
+            <SideBar @setMarker="setMarker"/>
         </v-container>
         <div id="mapid" ref="mapElement"></div>
     </div>
@@ -21,39 +21,61 @@ import SideBar from './components/drawer/SideBar';
         data() {
             return {
                 drawer: false,
+                location: {
+                  latitude: -20.880991,
+                  longitude: 55.449446
+                },
+              marker: undefined,
+              markerOptions: undefined,
+              mymap : undefined
             }
         },
         mounted() {
-            var mymap = L.map(this.$refs['mapElement']).setView([-20.880991, 55.449446], 13);
+          this.mymap = L.map(this.$refs['mapElement']).setView([-20.880991, 55.449446], 13)
 
-            L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoieW91diIsImEiOiJja2hlaHp6YnkwOHRqMzFwNWlmdmhtN3h4In0.e3EQyB9RFf547diSz_Xt0Q', {
-                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery :copyright: <a href="https://www.mapbox.com/">Mapbox</a>',
-                maxZoom:18,
-                id: 'mapbox/streets-v11',
-                tileSize: 512,
-                zoomOffset: -1,
-                accessToken: 'your.mapbox.access.token'
-            }).addTo(mymap);
+          L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoieW91diIsImEiOiJja2hlaHp6YnkwOHRqMzFwNWlmdmhtN3h4In0.e3EQyB9RFf547diSz_Xt0Q', {
+              attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery :copyright: <a href="https://www.mapbox.com/">Mapbox</a>',
+              maxZoom:18,
+              id: 'mapbox/streets-v11',
+              tileSize: 512,
+              zoomOffset: -1,
+              accessToken: 'your.mapbox.access.token'
+          }).addTo(this.mymap);
 
-            // Options for the marker
-            var markerOptions = {
-                title: "MyLocation",
-                clickable: true,
-                draggable: true
-            }
-            var marker = L.marker([-20.880991, 55.449446], markerOptions).addTo(mymap);
-            marker.draggable;
-            marker.bindPopup("<b>Hello world!</b><br>I am a popup.");
-            // var addressSearchResults = new L.LayerGroup().addTo(mymap);
+          // Options for the marker
+          this.markerOptions = {
+              title: "MyLocation",
+              clickable: true,
+              draggable: true
+          }
+          this.marker = L.marker([this.location.latitude, this.location.longitude], this.markerOptions).addTo(this.mymap);
+          // this.marker.draggable;
+          // this.marker.bindPopup("<b>Hello world!</b><br>I am a popup.");
+          // var addressSearchResults = new L.LayerGroup().addTo(mymap);
 
-            // On click show popup
-            // var popup = L.popup();
-            function onMapClick(e) {
-                marker = L.marker([e.latlng.lat, e.latlng.lng], markerOptions).addTo(mymap);
-            }
-            // marker.on('click', removeMarker);
-            mymap.on('click', onMapClick);
+          // On click show popup
+          // var popup = L.popup();
+          // function onMapClick(e) {
+          //     this.marker = L.marker([e.latlng.lat, e.latlng.lng], this.markerOptions).addTo(this.mymap);
+          // }
+          // marker.on('click', removeMarker);
+          // this.mymap.on('click', onMapClick);
+        },
+      methods : {
+        setMarker(location) {
+          let greenIcon = L.icon({
+            iconUrl: 'map-marker.png',
+              options: {
+                iconColor: 'green',
+              }
+          });
+          this.location = location
+
+          this.marker.remove()
+
+          this.marker = L.marker([this.location.latitude, this.location.longitude], {icon: greenIcon}).addTo(this.mymap);
         }
+      }
     }
 
 </script>
