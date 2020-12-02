172873de7d505d59f74f710c23d69fcba96656ae
florent etheve <fetheve2@gmail.com>
2020-12-02 14:27:49 +0400

    seeder carrefours

---
+++ Serveur/seeders/20201202102703-CarrefourSeed.js
@@ -0,0 +1,60 @@
+'use strict';
+var db=require("../models/index");
+const fs = require('fs');
+const turf = require('@turf/turf');
+const carrefour =require('../models/carrefour') ;
+
+const fichier =fs.readFileSync('./json/rue.geojson');
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */ 
+    let intersections=[];
+    let rues = JSON.parse(fichier);
+      
+    for (let i = 0; i < rues.features.length; i++) {
+      for (let y = 0; y < rues.features.length; y++){
+     let rue1=rues.features[i].geometry.coordinates
+     let rue2=rues.features[y].geometry.coordinates
+      var poly1 = turf.multiLineString(rue1);
+      var poly2 = turf.multiLineString(rue2);
+      var intersection = turf.intersect(poly1, poly2);
+      var type='lal';
+      if(intersection !== null){
+        if(intersection.geometry.type=='Point'){
+
+  var coord=intersection.geometry.coordinates;
+
+intersections.push({ 
+  geo:Sequelize.fn('ST_GeomFromText', `POINT(${coord[0]} ${coord[1]})`),
+    createdAt:new Date(),
+    updatedAt:new Date()
+});
+        }
+      }
+      
+      }
+    }
+
+  return queryInterface.bulkInsert('carrefours',intersections,{})
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    console.log("coord")
+    await queryInterface.bulkDelete('carrefours', null, {});
+  }
+};
